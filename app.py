c50af51e7f085c47c999da4ec3a493530cd103d4
Mohamed Nihal <nihalnowfal08@gmail.com>
2026-09-16 15:52:18 +0530

    Solved login page problem

--- app.py
+++ app.py
@@ -510,7 +510,6 @@ def logout():
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
-    flash("You have been signed out successfully.", "info")
     return response
 
 
